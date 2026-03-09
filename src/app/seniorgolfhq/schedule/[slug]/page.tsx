9243e117205c91e785dd0fe76d9f753ddb8fe4fd
chazymarketing <jonathan@chazymarketing.com>
2026-03-09 14:05:28 -0500

    feat: add TournamentHowToPlay to senior event pages

--- src/app/seniorgolfhq/schedule/[slug]/page.tsx
+++ src/app/seniorgolfhq/schedule/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { SiteShell } from "@/components/SiteShell";
 import { MajorEventHero } from "@/components/majors/MajorEventHero";
 import { MajorEventTabs } from "@/components/majors/MajorEventTabs";
+import { TournamentHowToPlay } from "@/components/tournaments/TournamentHowToPlay";
 import {
   getSeniorMajorBySlug,
   listSeniorMajorSlugs,
@@ -106,6 +107,7 @@ export default async function SeniorScheduleEventPage({
             resultsHref={`/seniorgolfhq/majors/${event.slug}/2026`}
             winners={event.winners ?? []}
           />
+            <TournamentHowToPlay howToPlay={event.howToPlay} />
         </div>
       </div>
     </SiteShell>
